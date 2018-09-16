38dfdd95c842e8591333892900d1121ac40783a6
Joseph Cobhams <tech.supreme1@gmail.com>
2018-09-17 00:31:38 +0100

    Added ability to see all routes registered within the application. Added come cosmetic surgery to the chow.py file.

--- run.py
+++ run.py
@@ -29,6 +29,30 @@ def create_db():
 @manager.command
 def drop_db():
 	db.drop_all()
+	
+@manager.command
+def show_routes():
+	from termcolor import colored
+	
+	count = 0
+	for rule in app.url_map.iter_rules():
+		methods = [m for m in rule.methods if m in ['GET','POST','PUT','PATCH','DELETE'] ]
+		line = 'Method: {method} | Route: {route} | Controller Endpoint: {endpoint}'.format(method=methods[0], route=rule, endpoint=rule.endpoint)
+		
+		if 'GET' in methods:
+			print(colored(line, 'green'), end='\n\n')
+			
+		if 'POST' in methods:
+			print(colored(line, 'yellow'), end='\n\n')
+			
+		if 'PUT' in methods or 'PATCH' in methods:
+			print(colored(line, 'grey'), end='\n\n')
+			
+		if 'DELETE' in methods:
+			print(colored(line, 'red'), end='\n\n')
+		
+		count+=1
+	print(count, 'Routes Found')
 
 if __name__ == '__main__':
 	manager.run()
