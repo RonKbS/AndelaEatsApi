8593d385da147776798295fc50d6513c5fff1fcd
nedemenang <nnamso.edemenang@gmail.com>
2018-09-27 16:47:52 +0100

    delete meal Item file

--- factories/meal_item_factory.py
+++ factories/meal_item_factory.py
@@ -2,7 +2,7 @@ import factory
 from app.utils import db
 from random import randint
 from app.utils.enums import MealTypes
-from app.models.meal import MealItem
+from app.models.meal_item import MealItem
 
 class MealItemFactory(factory.alchemy.SQLAlchemyModelFactory):
 
