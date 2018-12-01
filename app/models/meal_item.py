480e06788b169cf79cd9d0289cd8415e5fdf8a9c
Eno Bassey <ennyboy@gmail.com>
2018-12-01 22:19:34 +0100

    * upgrade to add location id in 3 models * upgrade meal item to have location id * upgrade menu item to have location id * upgrade order to have location id

--- app/models/meal_item.py
+++ app/models/meal_item.py
@@ -1,6 +1,7 @@
 from .base_model import BaseModel, db
 from app.utils.enums import MealTypes
 
+
 class MealItem(BaseModel):
     __tablename__ = 'meal_items'
 
@@ -8,3 +9,5 @@ class MealItem(BaseModel):
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(1000))
     image = db.Column(db.String(1000))
+    location_id = db.Column(db.Integer(), db.ForeignKey('locations.id'), default=1)
+    location = db.relationship('Location', lazy=False)
--- app/models/menu.py
+++ app/models/menu.py
@@ -3,16 +3,17 @@ from app.utils.enums import MealPeriods
 
 
 class Menu(BaseModel):
-	__tablename__ = 'menus'
+    __tablename__ = 'menus'
 
-	date = db.Column(db.Date(), nullable=False)
-	meal_period = db.Column(db.Enum(MealPeriods), nullable=False)
-	main_meal_id = db.Column(db.Integer(), db.ForeignKey('meal_items.id'), nullable=False)
-	allowed_side = db.Column(db.Integer(), default=1)
-	allowed_protein = db.Column(db.Integer(), default=1)
-	side_items = db.Column(db.String(), nullable=False)
-	protein_items = db.Column(db.String(), nullable=False)
-	vendor_engagement_id = db.Column(db.Integer(), db.ForeignKey('vendor_engagements.id'))
-
-	main_meal = db.relationship('MealItem', lazy=False)
-	vendor_engagement = db.relationship('VendorEngagement', lazy=False)
+    date = db.Column(db.Date(), nullable=False)
+    meal_period = db.Column(db.Enum(MealPeriods), nullable=False)
+    main_meal_id = db.Column(db.Integer(), db.ForeignKey('meal_items.id'), nullable=False)
+    allowed_side = db.Column(db.Integer(), default=1)
+    allowed_protein = db.Column(db.Integer(), default=1)
+    side_items = db.Column(db.String(), nullable=False)
+    protein_items = db.Column(db.String(), nullable=False)
+    vendor_engagement_id = db.Column(db.Integer(), db.ForeignKey('vendor_engagements.id'))
+    location_id = db.Column(db.Integer(), db.ForeignKey('locations.id'), default=1)
+    location = db.relationship('Location', lazy=False)
+    main_meal = db.relationship('MealItem', lazy=False)
+    vendor_engagement = db.relationship('VendorEngagement', lazy=False)
