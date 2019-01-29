be6ef22ac03dad604eb4fbec8385c70bbf895c70
Victor <vicads01@gmail.com>
2019-01-29 14:55:05 +0100

    add tests for util functions

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,46 @@
+from tests.base_test_case import BaseTestCase
+from datetime import datetime
+from app.utils import daterange, current_time_by_zone, check_date_current_vs_date_for
+
+
+class TestAuth(BaseTestCase):
+
+    def setUp(self):
+        self.BaseSetUp()
+
+    def test_daterange(self):
+        d1 = datetime.strptime('2019-01-01', '%Y-%m-%d')
+        d2 = datetime.strptime('2019-01-05', '%Y-%m-%d')
+        res_obj = daterange(d1, d2)
+        datelist = [item for item in res_obj]
+
+        self.assertEqual(len(datelist), 5)
+
+    def test_current_time_by_time_zone_positive_timezone(self):
+        res = current_time_by_zone('+3')
+
+        self.assertEqual(res.hour, datetime.utcnow().hour + 3)
+
+    def test_current_time_by_time_zone_negative_timezone(self):
+        resp = current_time_by_zone('-4')
+
+        self.assertEqual(resp.hour, datetime.utcnow().hour - 4)
+
+    def test_check_date_current_vs_date_for_invalid(self):
+        d1 = datetime.strptime('2019-01-01', '%Y-%m-%d')
+        d2 = datetime.strptime('2019-01-05', '%Y-%m-%d')
+
+        resp = check_date_current_vs_date_for(d1, d2)
+
+        self.assertFalse(resp)
+
+    def test_check_date_current_vs_date_for_valid(self):
+        d2 = datetime.strptime('2019-01-01', '%Y-%m-%d')
+        d1 = datetime.strptime('2019-01-05', '%Y-%m-%d')
+
+        resp = check_date_current_vs_date_for(d1, d2)
+
+        self.assertTrue(resp)
+
+
+
