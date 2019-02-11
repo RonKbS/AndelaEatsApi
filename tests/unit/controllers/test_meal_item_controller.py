ff2590f374436707945a0fc6547cf5a2e1b6b48f
Joseph Serunjogi <joeseggie@gmail.com>
2019-02-11 14:57:17 +0300

    Test list_meals OK response.

---
+++ tests/unit/controllers/test_meal_item_controller.py
@@ -0,0 +1,48 @@
+'''Unit tests for the meal item controller.
+'''
+from datetime import datetime
+from unittest.mock import patch
+
+from app.controllers.meal_item_controller import MealItemController
+from app.models import MealItem
+from app.repositories.meal_item_repo import MealItemRepo
+from app.utils.enums import MealTypes
+from tests.base_test_case import BaseTestCase
+
+
+class TestMealItemController(BaseTestCase):
+
+    def setUp(self):
+        self.BaseSetUp()
+        self.mock_meal_item = MealItem(
+            is_deleted=False,
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+            meal_type='Mock meal type',
+            name='Mock meal item',
+            description='Mock description',
+            image='Mock image',
+            location_id=1
+        )
+
+    @patch.object(MealItemRepo, 'get_unpaginated')
+    @patch('app.Auth.get_location')
+    def test_list_meals_ok_response(
+        self,
+        mock_get_location,
+        mock_get_unpaginated
+    ):
+        '''Test list_meals OK response.
+        '''
+        # Arrange
+        with self.app.app_context():
+            mock_get_location.return_value = 1
+            mock_get_unpaginated.return_value = [self.mock_meal_item, ]
+            meal_item_controller = MealItemController(self.request_context)
+
+            # Act
+            result = meal_item_controller.list_meals()
+
+            # Assert
+            assert result.status_code == 200
+            assert result.get_json()['msg'] == 'OK'
