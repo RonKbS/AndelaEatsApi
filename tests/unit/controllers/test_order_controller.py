7cdd40ed9c297350f2dd0a4012b7997640b0aba8
Joseph Serunjogi <joeseggie@gmail.com>
2019-02-12 15:39:46 +0300

    Test update_order when the order doesnot exist

--- tests/unit/controllers/test_order_controller.py
+++ tests/unit/controllers/test_order_controller.py
@@ -461,3 +461,31 @@ class TestOrderController(BaseTestCase):
             # Assert
             assert result.status_code == 201
             assert result.get_json()['msg'] == 'OK'
+
+    @patch('app.controllers.order_controller.OrderController.request_params')
+    @patch('app.repositories.meal_item_repo.MealItemRepo.get')
+    @patch('app.repositories.order_repo.OrderRepo.get')
+    def test_update_order_when_order_doesnot_exist(
+        self,
+        mock_order_repo_get,
+        mock_meal_item_repo_get,
+        mock_request_params
+    ):
+        '''Test update_order when the order doesnot exists.
+        '''
+        # Arrange
+        with self.app.app_context():
+            mock_request_params.return_value = (
+                '2019-02-12', 'web', []
+            )
+            mock_meal_item_repo_get.return_value = Mock()
+            mock_order_repo_get.return_value = None
+            order_controller = OrderController(self.request_context)
+
+            # Act
+            result = order_controller.update_order(1)
+
+            # Assert
+            assert result.status_code == 400
+            assert result.get_json()['msg'] == 'Invalid or incorrect order_id ' \
+                'provided'
