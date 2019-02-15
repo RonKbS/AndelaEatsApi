1a8a0d9cae850e464fe195758ca1261a58d65b29
Joseph Serunjogi <joeseggie@gmail.com>
2019-02-15 05:55:27 +0300

    Test un_suspend_vendor when vendor doesnot exist.

--- tests/unit/controllers/test_vendor_controller.py
+++ tests/unit/controllers/test_vendor_controller.py
@@ -289,3 +289,23 @@ class TestVendorController(BaseTestCase):
             # Assert
             assert result.status_code == 200
             assert result.get_json()['msg'] == 'OK'
+
+    @patch('app.repositories.vendor_repo.VendorRepo.get')
+    def test_un_suspend_vendor_when_vendor_doesnot_exist(
+        self,
+        mock_vendor_repo_get
+    ):
+        '''Test un_suspend_vendor when vendor doesnot exist.
+        '''
+        # Arrange
+        with self.app.app_context():
+            mock_vendor_repo_get.return_value = None
+            vendor_controller = VendorController(self.request_context)
+
+            # Act
+            result = vendor_controller.un_suspend_vendor(1)
+
+            # Assert
+            assert result.status_code == 400
+            assert result.get_json()['msg'] == 'Invalid or incorrect ' \
+                'vendor_id provided'
