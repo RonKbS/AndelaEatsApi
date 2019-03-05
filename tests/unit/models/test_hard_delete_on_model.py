0e2e91c27f448234205dfd226940d115d1e4f34c
roger <rogerokello@gmail.com>
2019-03-05 10:53:03 +0300

    ch(application-logs): Insert event listeners to Admin models
    - Add test to verify adding to table gets logged
    - Add test to verify updating table gets logged
    - Add test to verify hard deleting gets logged
    
    [Finishes #164240383]

--- tests/unit/models/test_hard_delete_on_model.py
+++ tests/unit/models/test_hard_delete_on_model.py
@@ -1,6 +1,6 @@
 from tests.base_test_case import BaseTestCase
 
-from app.models import Vendor
+from app.models import Vendor, Activity
 
 
 class TestHardDelete(BaseTestCase):
@@ -23,4 +23,49 @@ class TestHardDelete(BaseTestCase):
 
         self.assertEquals(Vendor.query.get(vendor_id), None)
 
+    def test_creating_vendor_gets_logged(self):
+        vendor = Vendor(
+            name='Your name',
+            address='Your Address',
+            tel='12345678',
+            contact_person='Contact Person',
+        )
+        vendor.save()
+
+        activity = [activity.action_details for activity in Activity.query.all()]
+
+        self.assertTrue("created" in activity[0])
+
+    def test_updating_vendor_gets_logged(self):
+        vendor = Vendor(
+            name='Your name',
+            address='Your Address',
+            tel='12345678',
+            contact_person='Contact Person',
+        )
+        vendor.save()
+
+        vendor = Vendor.query.get(1)
+        vendor.name = "New Name"
+        vendor.save()
+
+        activity = [activity.action_details for activity in Activity.query.all()]
+
+        self.assertTrue("updated" in activity[1])
+
+    def test_hard_deleting_vendor_gets_logged(self):
+        vendor = Vendor(
+            name='Your name',
+            address='Your Address',
+            tel='12345678',
+            contact_person='Contact Person',
+        )
+        vendor.save()
+
+        vendor = Vendor.query.get(1)
+        vendor.delete()
+
+        activity = [activity.action_details for activity in Activity.query.all()]
+
+        self.assertTrue("hard deleted" in activity[1])
 
