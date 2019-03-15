7a16e8c5e917987ba5cf08d3544f25b5796d9ea2
roger <rogerokello@gmail.com>
2019-03-15 16:11:03 +0300

    Add documentation for the about EPIC

--- app/blueprints/about_blueprint.py
+++ app/blueprints/about_blueprint.py
@@ -1,6 +1,8 @@
 """
 Module to deal with the about page
 """
+from flasgger import swag_from
+
 from app.blueprints.base_blueprint import Blueprint, BaseBlueprint, request
 from app.controllers.about_controller import AboutController
 
@@ -10,11 +12,13 @@ about_controller = AboutController(request)
 
 
 @about_blueprint.route('/view', methods=['GET'])
+@swag_from('documentation/get_about_page.yml')
 def get_about_page():
     return about_controller.get_about_page()
 
 
 @about_blueprint.route('/create_or_update', methods=['POST', 'PATCH'])
+@swag_from('documentation/create_about.yml')
 def create_about_page():
     return about_controller.create_or_modify_about_page()
 
