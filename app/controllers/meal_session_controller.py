24ede649ba9f0221f076b5721eb93f633f891db6
roger <rogerokello@gmail.com>
2019-03-21 18:52:24 +0300

    Add logic to the create_session method of the MealSessionController

--- app/controllers/meal_session_controller.py
+++ app/controllers/meal_session_controller.py
@@ -16,19 +16,27 @@ class MealSessionController(BaseController):
         name, start_time, end_time, date = self.request_params('name', 'startTime', 'endTime', 'date')
 
         start_time_split = start_time.split(":")
-        end_time_split = start_time.split(":")
+        end_time_split = end_time.split(":")
         date_split = date.split("-")
 
-        start_time = time(hour=start_time_split[0], minute=start_time_split[1])
-        end_time = time(hour=end_time_split[0], minute=end_time_split[1])
+        start_time = time(hour=int(start_time_split[0]), minute=int(start_time_split[1]))
+        end_time = time(hour=int(end_time_split[0]), minute=int(end_time_split[1]))
 
-        date = datetime(year=date_split[0], month=date_split[1], day=date_split[2])
+        date = datetime(year=int(date_split[0]), month=int(date_split[1]), day=int(date_split[2]))
 
         new_meal_session = self.meal_session_repo.new_meal_session(
             name=name, start_time=start_time, stop_time=end_time,
             date=date, location_id=location_id
         )
 
-        return self.handle_response('OK', payload={'mealSession': new_meal_session}, status_code=201)
+        new_meal_session.name = new_meal_session.name.value
 
+        new_meal_session.start_time = "".join(
+            [str(new_meal_session.start_time.hour), ":", str(new_meal_session.start_time.minute)])
+        new_meal_session.stop_time = "".join(
+            [str(new_meal_session.stop_time.hour), ":", str(new_meal_session.stop_time.minute)])
+
+        new_meal_session.date = new_meal_session.date.strftime("%Y-%m-%d")
+
+        return self.handle_response('OK', payload={'mealSession': new_meal_session.serialize()}, status_code=201)
 
