b3adced84bba24ab255dbfafe7c80d96dbca3937
roger <rogerokello@gmail.com>
2019-03-29 16:24:42 +0300

    ft(meal-session): Update meal session
    - Modify meal session controller create_meal_session method to reference
      error messages defined in the meal session repo
    - Modify meal session repo to have class method responsible for validation
      of update data sent
    - Add more class methods in meal session repo to cater for code duplication
    
    [Modifies #164679962]

--- app/controllers/meal_session_controller.py
+++ app/controllers/meal_session_controller.py
@@ -3,6 +3,7 @@ from flask import make_response, jsonify
 from datetime import datetime
 from app.controllers.base_controller import BaseController
 from app.repositories.meal_session_repo import MealSessionRepo
+from app.repositories.meal_service_repo import MealServiceRepo
 from app.utils.auth import Auth
 
 
@@ -11,6 +12,7 @@ class MealSessionController(BaseController):
     def __init__(self, request):
         BaseController.__init__(self, request)
         self.meal_session_repo = MealSessionRepo()
+        self.meal_service_repo = MealServiceRepo()
 
     def create_session(self):
         """
@@ -26,17 +28,19 @@ class MealSessionController(BaseController):
         if not location_id:
             location_id = Auth.get_location()
 
-        error_message_mapper = {
-            AttributeError: 'The location specified does not exist',
-            pytz.exceptions.UnknownTimeZoneError: 'The location specified is in an unknown time zone',
-            "invalid_time": 'The start time cannot be after end time',
-            "invalid_date": 'Date provided cannot be one before the current date',
-            "meal_session_exists_in_specified_time":
-                "This exact meal session already exists between the specified start and stop times",
-            "encloses_already_existing_meal_sessions":
-                "The start and stop times specified enclose one or more types of the same meal session",
-            "meal_session_already_exists": "This exact meal session already exists"
-        }
+        # error_message_mapper = {
+        #     AttributeError: 'The location specified does not exist',
+        #     pytz.exceptions.UnknownTimeZoneError: 'The location specified is in an unknown time zone',
+        #     "invalid_time": 'The start time cannot be after end time',
+        #     "invalid_date": 'Date provided cannot be one before the current date',
+        #     "meal_session_exists_in_specified_time":
+        #         "This exact meal session already exists between the specified start and stop times",
+        #     "encloses_already_existing_meal_sessions":
+        #         "The start and stop times specified enclose one or more types of the same meal session",
+        #     "meal_session_already_exists": "This exact meal session already exists"
+        # }
+
+        error_message_mapper = self.meal_session_repo.return_error_message_mapper()
 
         tz = self.meal_session_repo.get_location_time_zone(location_id)
         exception_message = error_message_mapper.get(tz)
@@ -116,77 +120,47 @@ class MealSessionController(BaseController):
         if not location_id:
             location_id = Auth.get_location()
 
-        error_message_mapper = {
-            AttributeError: 'The location specified does not exist',
-            pytz.exceptions.UnknownTimeZoneError: 'The location specified is in an unknown time zone',
-            "invalid_time": 'The start time cannot be after end time',
-            "invalid_date": 'Date provided cannot be one before the current date',
-            "meal_session_exists_in_specified_time":
-                "This exact meal session already exists between the specified start and stop times",
-            "encloses_already_existing_meal_sessions":
-                "The start and stop times specified enclose one or more types of the same meal session",
-            "meal_session_already_exists": "This exact meal session already exists"
+        meal_session_data = {
+            "name": name,
+            "start_time": start_time,
+            "end_time": end_time,
+            "date": date,
+            "location_id": location_id,
+            "meal_session_id": meal_session.id,
+            "meal_session": meal_session,
         }
 
-        tz = self.meal_session_repo.get_location_time_zone(location_id)
-        exception_message = error_message_mapper.get(tz)
-
-        if exception_message:
-            return make_response(jsonify({'msg': exception_message}), 400)
-
-        start_time = self.meal_session_repo.return_as_object(start_time, "time")
-        end_time = self.meal_session_repo.return_as_object(end_time, "time")
-
-        date_sent = self.meal_session_repo.return_as_object(date, "date")
-        current_date = datetime.now(tz)
-
-        message = self.meal_session_repo.validate_times_and_dates_not_greater_than_each_other(
-            start_time,
-            end_time,
-            datetime(year=current_date.year, month=current_date.month, day=current_date.day),
-            date_sent
+        validated_data = self.meal_session_repo.validate_update_of_meal_session(
+            **meal_session_data
         )
 
-        error_message = error_message_mapper.get(message)
+        error_message = validated_data.get("error_message")
 
         if error_message:
             return make_response(jsonify({'msg': error_message}), 400)
 
-        message = self.meal_session_repo.validate_update_meal_session_times(
-            **{
-                "name": name,
-                "date_sent": date_sent,
-                "location_id": location_id,
-                "start_time": start_time,
-                "end_time": end_time,
-                "meal_session_id": meal_session.id
-            }
-        )
-
-        message = error_message_mapper.get(message)
-
-        if message:
-            return make_response(jsonify({'msg': message}), 400)
-
-        new_meal_session = self.meal_session_repo.update_meal_session(
+        meal_session_updated = self.meal_session_repo.update_meal_session(
             meal_session,
-            name=name, start_time=start_time, stop_time=end_time,
-            date=date_sent, location_id=location_id
+            name=validated_data.get("name"),
+            start_time=validated_data.get("start_time"),
+            stop_time=validated_data.get("end_time"),
+            date=validated_data.get("date"),
+            location_id=validated_data.get("location_id")
         )
 
-        new_meal_session.name = new_meal_session.name.value
+        meal_session_updated.name = meal_session_updated.name.value
 
-        new_meal_session.start_time = self.meal_session_repo.get_time_as_string(
-            new_meal_session.start_time.hour,
-            new_meal_session.start_time.minute
+        meal_session_updated.start_time = self.meal_session_repo.get_time_as_string(
+            meal_session_updated.start_time.hour,
+            meal_session_updated.start_time.minute
         )
 
-        new_meal_session.stop_time = self.meal_session_repo.get_time_as_string(
-            new_meal_session.stop_time.hour,
-            new_meal_session.stop_time.minute
+        meal_session_updated.stop_time = self.meal_session_repo.get_time_as_string(
+            meal_session_updated.stop_time.hour,
+            meal_session_updated.stop_time.minute
         )
 
-        new_meal_session.date = new_meal_session.date.strftime("%Y-%m-%d")
+        meal_session_updated.date = meal_session_updated.date.strftime("%Y-%m-%d")
 
-        return self.handle_response('OK', payload={'mealSession': new_meal_session.serialize()}, status_code=201)
+        return self.handle_response('OK', payload={'mealSession': meal_session_updated.serialize()}, status_code=200)
 
--- app/repositories/meal_session_repo.py
+++ app/repositories/meal_session_repo.py
@@ -299,3 +299,125 @@ class MealSessionRepo(BaseRepo):
             date_sent
         ):
             return "invalid_date"
+
+    @classmethod
+    def validate_meal_created_after_or_on_current_date(cls, meal_session_date, current_date_with_tz):
+        """
+
+        :param meal_session_date: date meal session run
+        :param current_date_with_tz: current date aware of the time zone
+        :return: boolean
+        """
+
+        if cls.check_two_values_are_greater(
+            current_date_with_tz,
+            meal_session_date,
+        ):
+            return "meal session is before current date"
+
+    @staticmethod
+    def return_error_message_mapper():
+        """
+        Return a dictionary with all the error messages for the meal sessions
+        :return: string
+        """
+        return {
+            AttributeError: 'The location specified does not exist',
+            pytz.exceptions.UnknownTimeZoneError: 'The location specified is in an unknown time zone',
+            "invalid_time": 'The start time cannot be after end time',
+            "invalid_date": 'Date provided cannot be one before the current date',
+            "meal_session_exists_in_specified_time":
+                "This exact meal session already exists between the specified start and stop times",
+            "encloses_already_existing_meal_sessions":
+                "The start and stop times specified enclose one or more types of the same meal session",
+            "meal_session_already_exists": "This exact meal session already exists",
+            "meal session is before current date": "Cannot create a meal session before current date"
+        }
+
+    @classmethod
+    def validate_update_of_meal_session(cls, **kwargs):
+        """
+        Validates data sent from the update meal session endpoint
+        :param kwargs: dictionary keys are name, start_time, end_time, date, location_id, meal_session
+        :return:
+        """
+        validated_data = {
+            "error_message": ""
+        }
+
+        # Dictionary with the with the possible error messages
+        error_message_mapper = cls.return_error_message_mapper()
+
+        # Get time zone object for the location specified
+        tz = cls.get_location_time_zone(kwargs.get('location_id'))
+
+        exception_message = error_message_mapper.get(tz)
+
+        if exception_message:
+            validated_data["error_message"] = exception_message
+            return validated_data
+
+        start_time = cls.return_as_object(kwargs.get("start_time"), "time")
+        end_time = cls.return_as_object(kwargs.get("end_time"), "time")
+
+        date_sent = cls.return_as_object(kwargs.get("date"), "date")
+        current_date = datetime.now(tz)
+
+        message = cls.validate_times_and_dates_not_greater_than_each_other(
+            start_time,
+            end_time,
+            datetime(year=current_date.year, month=current_date.month, day=current_date.day),
+            date_sent
+        )
+
+        error_message = error_message_mapper.get(message)
+
+        if error_message:
+            validated_data["error_message"] = error_message
+            return validated_data
+
+        tz_stored_location = cls.get_location_time_zone(kwargs.get("meal_session").location_id)
+        current_date_using_stored_tz = datetime.now(tz_stored_location)
+
+        # check if meal session date is before current date(using the time zone information used to create it)
+        message = cls.validate_meal_created_after_or_on_current_date(
+            kwargs.get("meal_session").date,
+            datetime(
+                year=current_date_using_stored_tz.year,
+                month=current_date_using_stored_tz.month,
+                day=current_date_using_stored_tz.day
+            )
+        )
+
+        error_message = error_message_mapper.get(message)
+
+        if error_message:
+            validated_data["error_message"] = error_message
+            return validated_data
+
+        message = cls.validate_update_meal_session_times(
+            **{
+                "name": kwargs.get("name"),
+                "date_sent": date_sent,
+                "location_id": kwargs.get("location_id"),
+                "start_time": start_time,
+                "end_time": end_time,
+                "meal_session_id": kwargs.get("meal_session").id
+            }
+        )
+
+        message = error_message_mapper.get(message)
+
+        if message:
+            validated_data["error_message"] = message
+            return validated_data
+
+        validated_data = {
+            "name": kwargs.get("name"),
+            "date": date_sent,
+            "location_id": kwargs.get("location_id"),
+            "start_time": start_time,
+            "end_time": end_time,
+        }
+
+        return validated_data
