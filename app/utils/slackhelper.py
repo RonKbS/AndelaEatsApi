cabf12618fefd651330c13458376c3ddc90707f1
tainaandela <tolu.aina@andela.com>
2019-08-15 12:43:51 +0100

    slackclient 2.0 upgrade

--- app/utils/slackhelper.py
+++ app/utils/slackhelper.py
@@ -1,40 +1,67 @@
-from slackclient import SlackClient
+from slack import WebClient
+
 from config import get_env
 
 
 class SlackHelper:
-	
-	def __init__(self):
-		self.slack_token = get_env('SLACK_TOKEN')
-		self.slack_client = SlackClient(self.slack_token)
-	
-	def post_message(self, msg, recipient, attachments=None, as_user=True):
-		return self.slack_client.api_call(
-			"chat.postMessage",
-			channel=recipient,
-			text=msg,
-			attachments=attachments,
-			as_user=as_user
-		)
-	
-	def update_message(self, msg, recipient, message_ts=None, attachments=None):
-		return self.slack_client.api_call(
-			"chat.update",
-			channel=recipient,
-			ts=message_ts,
-			text=msg,
-			attachments=attachments,
-			as_user=True
-		)
-	
-	def user_info(self, uid):
-		return self.slack_client.api_call(
-			"users.info",
-			user=uid,
-		)
-	
-	def dialog(self, dialog, trigger_id):
-		return self.slack_client.api_call("dialog.open", trigger_id=trigger_id, dialog=dialog)
-	
-	def find_by_email(self, email):
-		return self.slack_client.api_call("users.lookupByEmail", email=email)
+
+    def __init__(self):
+        self.token = get_env('SLACK_TOKEN')
+        self.client = WebClient(self.token)
+
+    def post_message(self, message, channel, attachments=None, as_user=True):
+        """
+        Post a message to a public channel, private channel,
+        or direct message/IM channel.
+
+        https://api.slack.com/methods/chat.postMessage
+        """
+        return self.client.chat_postMessage(
+            channel=channel,
+            text=message,
+            attachments=attachments,
+            as_user=as_user
+        )
+
+    def update_message(self, message, channel, ts, attachments=None):
+        """
+        Update a message in a channel.
+
+        https://api.slack.com/methods/chat.update
+        """
+        return self.client.chat_update(
+            channel=channel,
+            ts=ts,
+            text=message,
+            attachments=attachments
+        )
+
+    def user_info(self, user):
+        """
+        Get information about a member of a workspace.
+
+        https://api.slack.com/methods/users.info
+        """
+        return self.client.users_info(
+            user=user
+        )
+
+    def dialog(self, dialog, trigger_id):
+        """Open a dialog with a user by exchanging a trigger_id.
+
+        https://api.slack.com/methods/dialog.open
+        """
+        return self.client.dialog_open(
+            dialog=dialog,
+            trigger_id=trigger_id
+        )
+
+    def find_by_email(self, email):
+        """
+        Retrieve a single user by looking them up by their email address.
+
+        https://api.slack.com/methods/users.lookupByEmail
+        """
+        return self.client.users_lookupByEmail(
+            email=email
+        )
